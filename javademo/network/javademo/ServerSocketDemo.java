68b4fbac7280178f98fde1673c4cec46aab058ce
jdl <jdl@JDL-DESKTOP>
2018-09-23 22:04:45 +0800

    增加了socket的例子

---
+++ javademo/network/javademo/ServerSocketDemo.java
@@ -0,0 +1,17 @@
+/*
+ * copyright 下午10:03:40
+ *
+ */
+package javademo;
+
+/**
+ * 
+ * 
+ * @author jdl
+ * 
+ * date: 2018年9月23日
+ *
+ */
+public class ServerSocketDemo {
+
+}
---
+++ javademo/network/javademo/Test.java
@@ -0,0 +1,19 @@
+/*
+ * copyright 下午10:03:53
+ *
+ */
+package javademo;
+
+/**
+ * 
+ * 
+ * @author jdl
+ * 
+ *         date: 2018年9月23日
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
